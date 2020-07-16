f23970a31305c1c8e8a753f063c49627fea26dec
angelique.a <angelique.assouvie@gmail.com>
2020-07-16 20:24:48 +0200

    add some tests and dtos

--- src/main/java/com/fr/sonarfilm/movie/controller/MovieController.java
+++ src/main/java/com/fr/sonarfilm/movie/controller/MovieController.java
@@ -1,7 +1,9 @@
  package com.fr.sonarfilm.movie.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fr.sonarfilm.movie.dto.MovieDTO;
 import com.fr.sonarfilm.movie.models.Movie;
 import com.fr.sonarfilm.movie.repositories.MovieRepository;
 import com.fr.sonarfilm.movie.traitement.AlgoMovieFinder;
@@ -20,37 +23,58 @@ public class MovieController {
 	
 	@Autowired
 	private MovieRepository movieRepository;
-
 	
-	@GetMapping(value = "/")
-	public List<Movie> getAllMovies() {
-	  return movieRepository.findAll();
+/*	private Movie convertToEntity(MovieDTO movieDto) {
+		Movie movie = modelMapper.map(movieDto, Movie.class);
+		return movie;
+	} */
+	
+	
+	private MovieDTO convertToDto(Movie movie) {
+		MovieDTO movieDto = new MovieDTO();
+		BeanUtils.copyProperties(movie, movieDto);
+		return movieDto;
 	}
 	
-	@GetMapping(value = "/{title}")
-	public List<Movie> getMovieByTitle(@PathVariable("title") String title) {
-	  return movieRepository.findByTitle(title);
+	
+	@GetMapping(value = "/")
+	public List<MovieDTO> getAllMovies() {
+		 List<Movie> movies = movieRepository.findAll();
+		 return movies
+				  .stream()
+		          .map(this::convertToDto)
+		          .collect(Collectors.toList());
+	}
+	
+	@GetMapping(value = "/title/{title}")
+	public List<MovieDTO> getMovieByTitle(@PathVariable("title") String title) {
+	  List<Movie> movies = movieRepository.findByTitle(title);
+	  return movies
+			  .stream()
+	          .map(this::convertToDto)
+	          .collect(Collectors.toList());
+			  
 	}
 	
 	@GetMapping(value = "/quizz/{firstGenre}")
-	public List<Movie> getMovieByFirstGenre(@PathVariable("firstGenre") String firstGenre) {
-	  return movieRepository.findByFirstGenre(firstGenre);
+	public List<MovieDTO> getMovieByFirstGenre(@PathVariable("firstGenre") String firstGenre) {
+		List<Movie> movies = movieRepository.findByFirstGenre(firstGenre);
+		return movies
+				  .stream()
+		          .map(this::convertToDto)
+		          .collect(Collectors.toList());
 	}
 	
 	
-	/*@GetMapping(value = "/quizz/{firstGenre}/{secondGenre}/{thirdGenre}/{fourthGenre}")
-	public List<Movie> getMovieByFirstGenreAndSecondGenre(@PathVariable("firstGenre") String firstGenre, @PathVariable("secondGenre") String secondGenre, @PathVariable("thirdGenre") String thirdGenre, @PathVariable("fourthGenre") String fourthGenre ) {
-	  return movieRepository.findByFirstGenreAndSecondGenreAndThirdGenreAndFourthGenre(firstGenre, secondGenre, thirdGenre, fourthGenre);
-	}
-	*/
-	
-	
-	
-	//requestbody ici et pas pathvariable
+
 	@GetMapping(value = "/quizz/{firstAnswer}/{secondAnswer}/{thirdAnswer}/{fourthAnswer}")
-	public List<Movie> processQuizz(@PathVariable("firstAnswer") String firstAnswer, @PathVariable("secondAnswer") String secondAnswer, @PathVariable("thirdAnswer") String thirdAnswer, @PathVariable("fourthAnswer") String fourthAnswer) {
+	public List<MovieDTO> processQuizz(@PathVariable("firstAnswer") String firstAnswer, @PathVariable("secondAnswer") String secondAnswer, @PathVariable("thirdAnswer") String thirdAnswer, @PathVariable("fourthAnswer") String fourthAnswer) {
 		List <Movie> quizzResults = new AlgoMovieFinder(movieRepository).whichMovie(firstAnswer, secondAnswer, thirdAnswer, fourthAnswer);
-		return quizzResults.subList(0, 21);
+		List <Movie> movies =  quizzResults.subList(0, 21);
+		return movies
+				  .stream()
+		          .map(this::convertToDto)
+		          .collect(Collectors.toList());
 	}
 	
 }
