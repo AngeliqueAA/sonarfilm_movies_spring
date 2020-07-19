e2b9864fc1e83d25336d25dfe5333912cac34a58
angelique.a <angelique.assouvie@gmail.com>
2020-07-19 23:13:09 +0200

    cleaning and read me

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
 # sonarfilm_movies_spring
 
-## test .git
+sonarfilm_movies_spring is a **spring boot** project which communicate with a mongoDB database and expose URL in a RESTAPI. 
+
+## Overview
+
+This project use movies data and do a simple algo to treat some data and give the best movie  depending on 4 criterias.
+
+Here a sample of the URI and the response :
+
+**GET** apimovies
+
+- return a list of all the movies present in DB
+
+**GET** apimovies/title/{title} 
+
+- return a list of movies which match with the given title
+
+**GET** apimovies/search/first/{firstGenre}
+
+- return a list of movies which match with the given genre (for example 'musical')
+
+## Installation 
+
+1. Make sure that you update the **application.properties** or the **run configuration** of Eclipse with your own credentials.
+
+2. Then, build the project and the tests with the following commands : 
+
+```
+mvn clean install  
+mvn verify
+mvn spring-boot:run
+
+```
+
+3. You can now use the project.
+
+
+## Related Project 
+
+The DB use by this project is filled by a NodeJS job : [sonarfilm_data](https://github.com/AngeliqueAA/sonarfilm_data_node)
--- src/main/java/com/fr/sonarfilm/movie/traitement/AlgoMovieFinder.java
+++ src/main/java/com/fr/sonarfilm/movie/traitement/AlgoMovieFinder.java
@@ -1,6 +1,5 @@
 package com.fr.sonarfilm.movie.traitement;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.fr.sonarfilm.movie.models.Movie;
@@ -21,40 +20,27 @@ public class AlgoMovieFinder {
 		int movieLimit = 20;
 		
 		
-		//round I with first genre in first
 		
 		List<Movie> results = movieRepository.findByFirstGenreAndSecondGenreAndThirdGenreAndFourthGenre(firstAnswer, secondAnswer, thirdAnswer, fourthAnswer);
 		List<Movie> almostPerfect = movieRepository.findByFirstGenreAndSecondGenreAndThirdGenreNot(firstAnswer, secondAnswer, thirdAnswer, fourthAnswer);
-//		List<Movie> nearlyPerfect = movieRepository.findByFirstGenreAndSecondGenreAndThirdGenreNot(firstAnswer, secondAnswer, fourthAnswer, thirdAnswer);
-//		List<Movie> match = movieRepository.findByFirstGenreAndSecondGenreNot(firstAnswer, secondAnswer, thirdAnswer, fourthAnswer);
 		List<Movie> endMatch = movieRepository.findByFirstGenreNotSecond(firstAnswer, secondAnswer);
 
-		//round I with second genre in first
+
 		
 		List<Movie> almostPerfectII = movieRepository.findByFirstGenreAndSecondGenreAndThirdGenreAndFourthGenre(secondAnswer, firstAnswer,  thirdAnswer, fourthAnswer);
 		List<Movie> nearlyPerfectII = movieRepository.findByFirstGenreAndSecondGenreAndThirdGenreNot(secondAnswer, firstAnswer, thirdAnswer, fourthAnswer);
-//		List<Movie> matchII = movieRepository.findByFirstGenreAndSecondGenreAndThirdGenreNot(secondAnswer, firstAnswer,  fourthAnswer, thirdAnswer);
-//		List<Movie> nearlyMatchII = movieRepository.findByFirstGenreAndSecondGenreNot(secondAnswer, firstAnswer, thirdAnswer, fourthAnswer);
-		
-		//List<Movie> rematchI = movieRepository.findByFirstGenreNot(firstAnswer, secondAnswer, thirdAnswer, fourthAnswer);
+
 		
 		
 		if (results.size()<movieLimit) {
 			results.addAll(almostPerfect);}
-/*		if (results.size()<movieLimit) {results.addAll(nearlyPerfect);}if (results.size()<movieLimit) {
-			results.addAll(match);}*/
 		if (results.size()<movieLimit) {results.addAll(endMatch);}
 		if (results.size()<movieLimit) {
 			results.addAll(almostPerfectII);
 		}if (results.size()<movieLimit) {
 			results.addAll(nearlyPerfectII);
-		}/* if (results.size()<movieLimit) {
-			results.addAll(matchII);
-		}if (results.size()<movieLimit) {
-			results.addAll(nearlyMatchII);
-		}*/
+		}
 		
-		//List<Movie> noOtherMatch = movieRepository.findByFirstGenre(firstAnswer);
 		List<Movie> noOtherMatch = movieRepository.findBySecondNotFirst(secondAnswer, firstAnswer);
 		
 		if(results.size()<movieLimit) {
@@ -71,13 +57,6 @@ public class AlgoMovieFinder {
 		
 		}
 
-	public List<Movie> whichGenreSecond(List<Movie> myMovies, String search){
 
-		myMovies = movieRepository.findBySecondGenre(search);
-
-		return myMovies;
-
-
-	}
 
 }
